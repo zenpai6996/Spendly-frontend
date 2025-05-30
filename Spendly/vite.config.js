0b2dea73b55237f20a89036df1c67a304fdf031a
SRB <souhardaroybarman05@gmail.com>
2025-05-30 14:00:51 +0530

    remove PWA plugin configuration from vite.config.js

--- Spendly/vite.config.js
+++ Spendly/vite.config.js
@@ -1,40 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Spendly',
-        short_name: 'Spendly',
-        description: 'Track your income and expenses effortlessly.',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#72CD16', 
-        icons: [
-          {
-            src: '/spendly.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/spendlylogo.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
